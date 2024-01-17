6f63e565e16481614a34511f491349fbd0a81bbf
mathslover1 <142133098+mathslover1@users.noreply.github.com>
2024-01-17 18:21:43 +0530

    new function

--- String _Functions.php
+++ String _Functions.php
@@ -56,5 +56,52 @@ $d = "Hello <br> World";
 // print_r(str_split($a));
 // Converts a string to an array, breaking it into smaller chunks.
 
-echo str_word_count($a);
+// echo str_word_count($a);
+// Returns the number of words in a string.
+
+// echo substr_replace($a, "123", 0, 0);
+// echo "<br>";
+// echo substr_replace($a, "123", 1, 3);
+// Replaces a portion of a string with another string.
+
+// echo str_pad($a, 15, "*", STR_PAD_BOTH);
+// echo "<br>";
+// echo str_pad($a, 15, "*", STR_PAD_LEFT);
+// echo "<br>";
+// echo str_pad($a, 15, "*", STR_PAD_RIGHT);
+// Pads a string to a certain length with another string
+
+$string1 = "abc";
+$string2 = "def";
+$ab="200";
+$ba="2";
+// echo strcmp($string1, $string2);
+// echo strcmp($ab, $ba);
+// 0 - if the two strings are equal
+// <0 - if string1 is less than string2
+// >0 - if string1 is greater than string2
+
+// echo strcoll($string1, $string2);
+// echo "<br>";
+// echo strcoll($ab, $ba);
+// Locale based string comparison.'
+
+// $mask = "123";
+// $mask2="bd";
+// echo strcspn($a, $mask,0,5);
+// echo "<br>";
+// echo strcspn($a, $mask2,3,5);
+
+// echo stristr($a, "DEF");
+// echo "<br>";
+// echo stristr($a, "b");
+// Case-insensitive search for the first occurrence of a string.
+
+// echo strpbrk($a, "ba");
+// Searches a string for any of a set of characters.
+
+// echo strrev($a);
+// Reverses a string
+
+
 ?>
