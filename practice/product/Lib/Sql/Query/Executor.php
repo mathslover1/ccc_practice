53e249c3fcda33ddf49b17666f8ccd7a3151bbc9
mathslover1 <142133098+mathslover1@users.noreply.github.com>
2024-02-08 13:46:32 +0530

    add new things

--- practice/product/Lib/Sql/Query/Executor.php
+++ practice/product/Lib/Sql/Query/Executor.php
@@ -1,30 +1,32 @@
 <pre>
 <?php
-class Lib_Sql_Query_Executor extends Lib_Sql_Connection{
+class Lib_Sql_Query_Executor extends Lib_Sql_Connection
+{
     public function execute($sql)
     {
-    	try {
-    		return $this->connect()->query($sql);
-            
-    	} catch(Exception $e) {
+        try {
+            return $this->connect()->query($sql);
+        } catch (Exception $e) {
 
-    		var_dump($e->getMessage());
-    	}
-    }
-    public function fetchAssoc($result){
-        $data=[];
-        if($result->num_rows>0){
-            while($row = $result->fetch_assoc()){
-                $data[]=$row;
-            }
+            var_dump($e->getMessage());
         }
-        return $data;
     }
-    public  function fetchRow($result){
-        if($result->num_rows>0){
-            $row=$result->fetch_assoc();
+    public function fetchAssoc($result)
+    {
+        $data = new Model_Data_Collection();
+        while ($row = $result->fetch_assoc()) {
+            $data->addData($row);
+        }
+        if (count($data->getData()) == 0) {
+            return null;
+        }
+        return count($data->getData()) > 1 ? $data->getData() : $data->getData()[0];
+    }
+    public  function fetchRow($result)
+    {
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
             return $row;
-
         }
     }
 }
--- practice/product/View/Product.php
+++ practice/product/View/Product.php
@@ -1,21 +1,20 @@
 <pre><?php
-class View_Product
-{
-    public $newobj;
-    public function __construct()
-    {
-        $obj = new Model_Request();
-        $obj->getGetData('id');
-        $id = ($obj->getGetData('id'));
-        $obj = new Model_Abstract();
-        $sql = $obj->getQueryBuilder()->select("ccc_product", "*", ["product_id" => $id]);
-        $test = $obj->getQueryExecutor()->execute($sql);
-        $data = $obj->getQueryExecutor()->fetchAssoc($test);
-        $this->newobj = new Model_Data_Object($data);
-        
-    }
-    public function createForm(){
-        $form = '<style>
+        class View_Product
+        {
+            public $newobj;
+            public function __construct()
+            {
+                $obj = new Model_Request();
+                $id = ($obj->getGetData('id'));
+                $obj = new Model_Abstract();
+                $sql = $obj->getQueryBuilder()->select("ccc_product", "*", ["product_id" => $id]);
+                $test = $obj->getQueryExecutor()->execute($sql);
+                $data = $obj->getQueryExecutor()->fetchAssoc($test);
+                $this->newobj = $data;
+            }
+            public function createForm($newObj)
+            {
+                $form = '<style>
                     .form-container {
                         width: 60%;
                         margin: 0 auto;
@@ -46,99 +45,145 @@ class View_Product
                         margin-right: 5px;
                     }
                 </style>';
+                $form .= '<div class="form-container">';
+                $form .= '<form action="" method="POST" class="product-form">';
+                $form .= '<div class="form-group">';
+                if ($newObj) {
+                    $form .= $this->creteTextField('ccc_product[product_name]', "Product Name:", $newObj->getproduct_name(), 'product_name', 'form-control');
+                    $form .= '</div>';
+                    $form .= '<div class="form-group">';
+                    $form .= $this->creteTextField('ccc_product[sku]', "Sku:", $newObj->getsku(), 'sku', 'form-control');
+                    $form .= '</div>';
+                    $form .= '<div class="form-group">';
+                    $form .= $this->createRadioButtons('ccc_product[product_type]', "Product Type:", array(
+                        'simple' => 'Simple',
+                        'bundle' => 'Bundle',
+                    ), $newObj->getproduct_type(), '');
+                    $form .= '</div>';
+                    $form .= '<div class="form-group">';
+                    $form .= $this->createDropdownField('ccc_product[cat_id]', "Category:", array(
+                        '1' => 'Bar & Game Room',
+                        '2' => 'Bedroom',
+                        '3' => 'Decor',
+                        '4' => 'Dining & Kitchen',
+                        '5' => 'Lighting',
+                        '6' => 'Living Room',
+                        '7' => 'Mattresses',
+                        '8' => 'Office',
+                        '9' => 'Outdoor',
+                    ), $newObj->getcat_id(), 'category', 'form-control');
+                    $form .= '</div>';
+                    $form .= '<div class="form-group">';
+                    $form .= $this->creteTextField('ccc_product[manufacturer_cost]', "Manufacturer Cost:", $newObj->getmanufacturer_cost(), 'manufacturer_cost', 'form-control');
+                    $form .= '</div>';
+                    $form .= '<div class="form-group">';
+                    $form .= $this->creteTextField('ccc_product[shipping_cost]', "Shipping Cost:", $newObj->getshipping_cost(), 'shipping_cost', 'form-control');
+                    $form .= '</div>';
+                    $form .= '<div class="form-group">';
+                    $form .= $this->creteTextField('ccc_product[total_cost]', "Total Cost:", $newObj->gettotal_cost(), 'total_cost', 'form-control');
+                    $form .= '</div>';
+                    $form .= '<div class="form-group">';
+                    $form .= $this->creteTextField('ccc_product[price]', "Price:", $newObj->getprice(), 'price', 'form-control');
+                    $form .= '</div>';
+                    $form .= '<div class="form-group">';
+                    $form .= $this->createDropdownField('ccc_product[status]', "Status:", [
+                        'enabled' => 'Enabled',
+                        'disabled' => 'Disabled',
+                    ], $newObj->getstatus(), 'status', 'form-control');
+                    $form .= '</div>';
+                } else {
+                    $form .= $this->creteTextField('ccc_product[product_name]', "Product Name:", '', 'product_name', 'form-control');
+                    $form .= '</div>';
+                    $form .= '<div class="form-group">';
+                    $form .= $this->creteTextField('ccc_product[sku]', "Sku:", '', 'sku', 'form-control');
+                    $form .= '</div>';
+                    $form .= '<div class="form-group">';
+                    $form .= $this->createRadioButtons('ccc_product[product_type]', "Product Type:", array(
+                        'simple' => 'Simple',
+                        'bundle' => 'Bundle',
+                    ), '', '');
+                    $form .= '</div>';
+                    $form .= '<div class="form-group">';
+                    $form .= $this->createDropdownField('ccc_product[cat_id]', "Category:", array(
+                        '1' => 'Bar & Game Room',
+                        '2' => 'Bedroom',
+                        '3' => 'Decor',
+                        '4' => 'Dining & Kitchen',
+                        '5' => 'Lighting',
+                        '6' => 'Living Room',
+                        '7' => 'Mattresses',
+                        '8' => 'Office',
+                        '9' => 'Outdoor',
+                    ), '', 'category', 'form-control');
+                    $form .= '</div>';
+                    $form .= '<div class="form-group">';
+                    $form .= $this->creteTextField('ccc_product[manufacturer_cost]', "Manufacturer Cost:", '', 'manufacturer_cost', 'form-control');
+                    $form .= '</div>';
+                    $form .= '<div class="form-group">';
+                    $form .= $this->creteTextField('ccc_product[shipping_cost]', "Shipping Cost:", '', 'shipping_cost', 'form-control');
+                    $form .= '</div>';
+                    $form .= '<div class="form-group">';
+                    $form .= $this->creteTextField('ccc_product[total_cost]', "Total Cost:", '', 'total_cost', 'form-control');
+                    $form .= '</div>';
+                    $form .= '<div class="form-group">';
+                    $form .= $this->creteTextField('ccc_product[price]', "Price:", '', 'price', 'form-control');
+                    $form .= '</div>';
+                    $form .= '<div class="form-group">';
+                    $form .= $this->createDropdownField('ccc_product[status]', "Status:", [
+                        'enabled' => 'Enabled',
+                        'disabled' => 'Disabled',
+                    ], '', 'status', 'form-control');
+                    $form .= '</div>';
+                }
+                $form .= '<div class="form-group">';
+                $form .= $this->creteSubmitBtn('Submit');
+                $form .= '</div>';
+                $form .= '</form>';
+                $form .= '</div>';
+                return $form;
+            }
 
-        $form .= '<div class="form-container">';
-        $form .= '<form action="" method="POST" class="product-form">';
-        $form .= '<div class="form-group">';
-        $form .= $this->creteTextField('ccc_product[product_name]', "Product Name:",$this->newobj->getproduct_name(), 'product_name', 'form-control');
-        $form .= '</div>';
-        $form .= '<div class="form-group">';
-        $form .= $this->creteTextField('ccc_product[sku]', "Sku:",$this->newobj->getsku(), 'sku', 'form-control');
-        $form .= '</div>';
-        $form .= '<div class="form-group">';
-        $form .= $this->createRadioButtons('ccc_product[product_type]',"Product Type:", array(
-            'simple' => 'Simple',
-            'bundle' => 'Bundle',
-        ),$this->newobj->getproduct_type(), '');
-        $form .= '</div>';
-        $form .= '<div class="form-group">';
-        $form .= $this->createDropdownField('ccc_product[cat_id]',"Category:", array(
-            '1' => 'Bar & Game Room',
-            '2' => 'Bedroom',
-            '3' => 'Decor',
-            '4' => 'Dining & Kitchen',
-            '5' => 'Lighting',
-            '6' => 'Living Room',
-            '7' => 'Mattresses',
-            '8' => 'Office',
-            '9' => 'Outdoor',
-        ),$this->newobj->getcat_id(), 'category', 'form-control');
-        $form .= '</div>';
-        $form .= '<div class="form-group">';
-        $form .= $this->creteTextField('ccc_product[manufacturer_cost]', "Manufacturer Cost:",$this->newobj->getmanufacturer_cost(), 'manufacturer_cost', 'form-control');
-        $form .= '</div>';
-        $form .= '<div class="form-group">';
-        $form .= $this->creteTextField('ccc_product[shipping_cost]', "Shipping Cost:",$this->newobj->getshipping_cost(), 'shipping_cost', 'form-control');
-        $form .= '</div>';
-        $form .= '<div class="form-group">';
-        $form .= $this->creteTextField('ccc_product[total_cost]', "Total Cost:",$this->newobj->gettotal_cost(), 'total_cost', 'form-control');
-        $form .= '</div>';
-        $form .= '<div class="form-group">';
-        $form .= $this->creteTextField('ccc_product[price]', "Price:",$this->newobj->getprice(), 'price', 'form-control');
-        $form .= '</div>';
-        $form .= '<div class="form-group">';
-        $form .= $this->createDropdownField('ccc_product[status]',"Status:",[
-            'enabled' => 'Enabled',
-            'disabled' => 'Disabled',
-        ],$this->newobj->getstatus(), 'status', 'form-control');
-        $form .= '</div>';
-        $form .= '<div class="form-group">';
-        $form .= $this->creteSubmitBtn('Submit');
-        $form .= '</div>';
-        $form .= '</form>';
-        $form .= '</div>';
-        return $form;
-    }
+            public function creteTextField($name, $title, $value = '', $id = '', $class = '')
+            {
+                return '<span> ' . $title . ' </span><input id="' . $id . '" class="' . $class . '" type="text" name="' . $name . '" value="' . $value . '">';
+            }
 
-    public function creteTextField($name, $title, $value = '', $id = '', $class = '')
-    {
-        return '<span> ' . $title . ' </span><input id="' . $id . '" class="' . $class . '" type="text" name="' . $name . '" value="' . $value . '">';
-    }
+            public function createDropdownField($name, $title, $options = array(), $selected = '', $id = '', $class = '')
+            {
+                $dropdown = $title;
+                $dropdown .= '<select id="' . $id . '" class="' . $class . '" name="' . $name . '">';
 
-    public function createDropdownField($name,$title,$options = array(), $selected = '', $id = '', $class = '') {
-        $dropdown = $title;
-        $dropdown .= '<select id="' . $id . '" class="' . $class . '" name="' . $name . '">';
+                foreach ($options as $key => $value) {
+                    $selectedAttr = ($key == $selected) ? 'selected="selected"' : '';
+                    $dropdown .= '<option value="' . $key . '" ' . $selectedAttr . '>' . $value . '</option>';
+                }
 
-        foreach ($options as $key => $value) {
-            $selectedAttr = ($key == $selected) ? 'selected="selected"' : '';
-            $dropdown .= '<option value="' . $key . '" ' . $selectedAttr . '>' . $value . '</option>';
+                $dropdown .= '</select>';
+                return $dropdown;
+            }
+
+            public function createRadioButtons($name, $title, $options = array(), $selected = '', $id = '')
+            {
+                $radios = '';
+                $radios .= $title;
+                $radios .= "<br>";
+                foreach ($options as $value => $label) {
+                    $checkedAttr = ($value == $selected) ? 'checked="checked"' : '';
+                    $radios .= '<label for="' . $id . '_' . $value . '">' . $label . '</label>';
+                    $radios .= '<input type="radio" id="' . $id . '_'  . '" name="' . $name . '" value="' . $value . '" ' . $checkedAttr . '>';
+                }
+
+                return $radios;
+            }
+
+            public function creteSubmitBtn($title)
+            {
+                return '<input type="submit" class="btn btn-primary" name="submit" value="' . $title . '">';
+            }
+
+            public function toHTML()
+            {
+                return $this->createForm($this->newobj);
+            }
         }
-
-        $dropdown .= '</select>';
-        return $dropdown;
-    }
-
-    public function createRadioButtons($name,$title,$options = array(),$selected = '', $id = '') {
-        $radios ='';
-        $radios .=$title;
-        $radios.= "<br>";
-        foreach ($options as $value => $label) {
-            $checkedAttr = ($value == $selected) ? 'checked="checked"' : '';
-            $radios .= '<label for="' . $id . '_' . $value . '">' . $label . '</label>';
-            $radios .= '<input type="radio" id="' . $id . '_'  . '" name="' . $name . '" value="' . $value . '" ' . $checkedAttr . '>';
-        }
-
-        return $radios;
-    }
-
-    public function creteSubmitBtn($title)
-    {
-        return '<input type="submit" class="btn btn-primary" name="submit" value="'.$title.'">';
-    }
-
-    public function toHTML()
-    {
-        return $this->createForm();
-    }
-}
-?>
+        ?>
